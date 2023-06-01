3dc3c389442d47600805ee4db94227d4cc38bb15
Annu Yadav <126255136+annuyadav31@users.noreply.github.com>
2023-06-01 17:55:28 +0100

    Update DiaryEntry.cs

--- webApi/Models/DiaryEntry.cs
+++ webApi/Models/DiaryEntry.cs
@@ -3,8 +3,8 @@
     public class DiaryEntry
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public DateTime CreationTime { get; set; }
-        public string Content { get; set; }
+        public string? Content { get; set; }
     }
 }
